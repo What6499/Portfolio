38fdb9abeb6402cda4b297d0344c08d790bd4083
Jamil Ahmed <what6499@gmai.com>
2025-08-12 19:21:35 +0600

    first commit

---
+++ my-portfolio/src/Routes/route.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+import App from "../App";
+
+const router = createBrowserRouter([
+  {
+    index: true,
+    Component: App,
+  },
+]);
+export default router;
---
+++ my-portfolio/src/components/Navbar.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { Link } from "react-router";
+
+const Navbar = () => {
+  return (
+    <div className=" bg-[#1B1B1B]  text-white font-bold  shadow-xl">
+      <div className="container navbar mx-auto">
+        <div className="navbar-start ">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow  rounded-box w-52"
+            >
+              <li>
+                <Link to="#home">Home</Link>
+              </li>
+              <li>
+                <Link to="#about">About</Link>
+              </li>
+              <li>
+                <Link to="#skills">Skills</Link>
+              </li>
+              <li>
+                <Link to="#projects">Projects</Link>
+              </li>
+              <li>
+                <Link to="#contacts">Contacts</Link>
+              </li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">My Portfolio</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu space-x-4 menu-horizontal px-1">
+            <li>
+              <Link to="#home">Home</Link>
+            </li>
+            <li>
+              <Link to="#about">About</Link>
+            </li>
+            <li>
+              <Link to="#skills">Skills</Link>
+            </li>
+            <li>
+              <Link to="#projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="#contacts">Contacts</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Resume</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
